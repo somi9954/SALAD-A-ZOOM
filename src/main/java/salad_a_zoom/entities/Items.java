eda998abd9a64cbd7928fd7bbeb5914b9c07ebe1
진혁 <kitty0888@naver.com>
2023-12-19 20:09:48 +0900

    엔티티 설정..

--- src/main/java/salad_a_zoom/entities/Items.java
+++ src/main/java/salad_a_zoom/entities/Items.java
@@ -1,4 +1,57 @@
 package salad_a_zoom.entities;
 
-public class Items {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity @Data @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Items extends Board {
+    @Id
+    @Column(length=50)
+    private String sId;  // bId를 sId로 변경
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="board_bId")
+    private Board board;
+
+    @Id
+    private Long seq;
+
+    @Id @Column(length = 50, nullable = false)
+    private String itemNm;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="admin_userNo")
+    private Admin admin;
+
+    private Long stockNumber;
+
+    @Column(nullable = false)
+    private Long price;
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="fileInfo_id")
+    @Column(length = 100, nullable = false)
+    private FileInfo fileInfo;
+
+    @Transient
+    private String filePath;
+
+    @Transient
+    private String fileUrl;
+
+    @Transient
+    private String[] thumbsPath;
+
+    @Transient
+    private String[] thumbsUrl;
+
 }
