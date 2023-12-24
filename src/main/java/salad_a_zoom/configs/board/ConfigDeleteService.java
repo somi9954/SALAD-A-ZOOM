d73f114c97a992971939a774ccb9d10c2d5022b4
somi9954 <jolh999@gmail.com>
2023-12-25 04:21:23 +0900

    :memo: board 공통 설정 추가

---
+++ src/main/java/salad_a_zoom/configs/board/ConfigDeleteService.java
@@ -0,0 +1,20 @@
+package salad_a_zoom.configs.board;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ConfigDeleteService {
+    private final ConfigsRepository repository;
+
+    public void delete(String code) {
+        Configs configs = repository.findById(code).orElse(null);
+        if (configs == null) {
+            return;
+        }
+
+        repository.delete(configs);
+        repository.flush();
+    }
+}
